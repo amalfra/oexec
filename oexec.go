252247575918e796d629d4c67aaaa38b7caa3482
Amal Francis <amalfra@gmail.com>
2017-08-19 19:12:33 +0530

    add Parallel mode

--- oexec.go
+++ oexec.go
@@ -3,18 +3,44 @@ package oexec
 import (
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 // Series will execute specified cmds in series and return slice of Outputs
 func Series(cmds ...string) []*Output {
 	outputs := make([]*Output, len(cmds))
 	for index, cmd := range cmds {
-		cmdName, cmdArgs := processCmdStr(cmd)
-		outStruct := new(Output)
-		outStruct.Stdout, outStruct.Stderr = execute(cmdName, cmdArgs...)
-		outputs[index] = outStruct
+		outputs[index] = run(cmd)
 	}
 	return outputs
+}
+
+// Parallel will execute specified cmds in parallel and return slice of
+// Outputs when all of them have completed
+func Parallel(cmds ...string) []*Output {
+	var wg sync.WaitGroup
+	outputs := make([]*Output, len(cmds))
+	for index, cmd := range cmds {
+		wg.Add(1)
+		go runParallel(&wg, cmd, index, outputs)
+	}
+	wg.Wait()
+	return outputs
+}
+
+// runParallel is a helper go routine to run cmd in async
+// and put result into array
+func runParallel(wg *sync.WaitGroup, cmd string, index int, outputs []*Output) {
+	defer wg.Done()
+	outputs[index] = run(cmd)
+}
+
+// run will process the specified cmd, execute it and return the Output struct
+func run(cmd string) *Output {
+	cmdName, cmdArgs := processCmdStr(cmd)
+	outStruct := new(Output)
+	outStruct.Stdout, outStruct.Stderr = execute(cmdName, cmdArgs...)
+	return outStruct
 }
 
 // processCmdStr will split full command string to command and arguments slice
